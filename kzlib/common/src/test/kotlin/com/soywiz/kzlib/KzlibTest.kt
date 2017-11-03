71807b5366da8fd976c4677c4b367ea0b571da8e
soywiz <soywiz@gmail.com>
2017-11-04 00:50:52 +0100

    Small adjustment

--- kzlib/common/src/test/kotlin/com/soywiz/kzlib/KzlibTest.kt
+++ kzlib/common/src/test/kotlin/com/soywiz/kzlib/KzlibTest.kt
@@ -1,11 +1,15 @@
 package com.soywiz.kzlib
 
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class KzlibTest {
 	@Test
-	fun name() {
-		val res = "HELLO HELLO HELLO HELLO WORLD".toSimpleByteArray().deflate()
+	fun testDeflateInflate() {
+		val original = "HELLO HELLO HELLO HELLO WORLD".toSimpleByteArray()
+		val compressed = original.deflate()
+		val uncompressed = compressed.inflate()
 
+		assertEquals(original.toList(), uncompressed.toList())
 	}
 }
